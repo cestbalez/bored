4941e2491f6dcdae597d8209c00e8b1dfee114cf
Graham Sundman <grahamsundman@hotmail.com>
2018-08-02 12:05:05 +0800

    added reference key between board and user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_01_103055) do
+ActiveRecord::Schema.define(version: 2018_08_02_034413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 2018_08_01_103055) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_boards_on_user_id"
   end
 
   create_table "bookings", force: :cascade do |t|
@@ -35,6 +37,17 @@ ActiveRecord::Schema.define(version: 2018_08_01_103055) do
     t.datetime "updated_at", null: false
     t.index ["board_id"], name: "index_bookings_on_board_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.integer "rating"
+    t.bigint "user_id"
+    t.bigint "board_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["board_id"], name: "index_reviews_on_board_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -57,4 +70,6 @@ ActiveRecord::Schema.define(version: 2018_08_01_103055) do
 
   add_foreign_key "bookings", "boards"
   add_foreign_key "bookings", "users"
+  add_foreign_key "reviews", "boards"
+  add_foreign_key "reviews", "users"
 end
