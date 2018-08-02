406bc7f50b6a5e3103e2dbc7a0158009bf3f13af
Magnus Remoe <mremoe@gmail.com>
2018-08-02 12:55:03 +0800

    added validations to models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,8 @@ class Booking < ApplicationRecord
   belongs_to :board
   belongs_to :user
 
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :user_id, presence: true
+  validates :board_id, presence: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,9 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :board
+
+  validates :content, presence: true
+  validates :rating, presence: true, inclusion: { in: [1, 2, 3, 4, 5] }
+  validates :user_id, presence: true
+  validates :board_id, presence: true
 end
