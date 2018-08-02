41023e446879d44a80f51591cd1911db06dcf0a8
Magnus Remoe <mremoe@gmail.com>
2018-08-02 12:20:47 +0800

    added reviews to router

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :boards do
     resources :bookings, only: [:new, :create, :index]
+    resources :reviews, only: [:new, :create, :index]
   end
 end
