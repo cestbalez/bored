465fe14e78c816f30b5c6de593eec51c4e481d6c
Valentin Raille <Valrail78@gmail.com>
2018-08-06 17:45:50 +0800

    added the reviews controller and implemented in the view, need to create the form now

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,23 @@
+class ReviewsController < ApplicationController
+
+  def new
+    @board = Board.find(params[:board_id])
+    @review = Review.new
+    authorize @review
+  end
+
+  def create
+    @board = Board.find(params[:board_id])
+    @review = Review.new(review_params)
+    @review.board = @board
+    @review.save
+    redirect_to board_path(@board)
+    authorize @review
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit( :content, :rating, :user_id, :board_id )
+  end
+end
