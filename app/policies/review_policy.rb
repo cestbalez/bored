17db3353a0981d7bdc33feaa85f8860f1ac73d26
Valentin Raille <Valrail78@gmail.com>
2018-08-06 19:45:51 +0800

    Implement the review form in the reviews view

--- app/policies/review_policy.rb
+++ app/policies/review_policy.rb
@@ -4,4 +4,24 @@ class ReviewPolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  def create?
+    true
+  end
+
+  def new?
+    create?
+  end
+
+  # def edit?
+  #   update?
+  # end
+
+  # def update?
+  #   is_user?
+  # end
+
+  # def destroy?
+  #   is_user?
+  # end
 end
