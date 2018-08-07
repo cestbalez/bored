81b2f97d7ef9d338edc339f557e080fec518c747
Graham Sundman <grahamsundman@hotmail.com>
2018-08-07 08:26:42 +0800

    added create?, new? and is_user? to ReviewPolicy

--- app/policies/review_policy.rb
+++ app/policies/review_policy.rb
@@ -4,4 +4,17 @@ class ReviewPolicy < ApplicationPolicy
       scope.all
     end
   end
+  def create?
+    true
+  end
+
+  def new?
+    create?
+  end
+
+    private
+
+  def is_user?
+    record.user == user
+  end
 end
